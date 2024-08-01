9170c11635ec50afdb1b0c8e89d96698c558304f
Bartłomiej Skura <bartek33s@wp.pl>
2024-08-01 16:45:33 +0200

    Add validation for forms in API

---
+++ server/src/services/createForm/validator.ts
@@ -0,0 +1,42 @@
+import { Validator, Schema } from "jsonschema";
+
+import { BadRequestError } from "../../errors/BadRequestError";
+import { Continent } from "../getContinents/continents";
+import { isFuture } from "date-fns";
+
+export const validate = (body: any) => {
+  const validator = new Validator();
+
+  const validation = validator.validate(body, validationSchema);
+  
+  if (!validation.valid) {
+    throw new BadRequestError("Invalid request body");
+  }
+  if(body.continent === Continent.Europe && (!body.lastName || body.lastName.length < 2)){
+    throw new BadRequestError("Last name cannot be shorter than 2 characters for Europe")
+  }
+  if(isFuture(body.birthdate)){
+    throw new BadRequestError("Birthdate cannot be in the future");
+  }
+};
+
+const validationSchema: Schema = {
+  type: "object",
+  properties: {
+    firstName: {
+      type: "string",
+      required: true
+    },
+    lastName: {
+      type: ["string", "null"],
+    },
+    birthdate: {
+      type: ["string", "null"],
+      format: "date",
+    },
+    continent: {
+      type: ["string", "null"],
+      enum: [...Object.values(Continent)],
+    },
+  },
+};
