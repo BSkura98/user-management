c33effa6ae35cf55cab3afbd10600afc87b416a7
Bartłomiej Skura <bartek33s@wp.pl>
2024-08-03 16:11:26 +0200

    Remove unused file

--- client/src/utils/userAge.ts
+++ client/src/utils/userAge.ts
@@ -1,9 +1,5 @@
 import dayjs, { Dayjs } from "dayjs";
 
-export const setUserBirthdate = (birthdate: string) => {
-  localStorage.setItem("birthdate", birthdate);
-};
-
 export const isUserOver60 = (birthdate: string | Dayjs | null) => {
   if (birthdate) {
     const differenceInYears = dayjs().diff(dayjs(birthdate), "year");
